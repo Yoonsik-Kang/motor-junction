193ad7fffdca4acf5e06e9a74d9cfbb17a963103
Yoonsik-Kang <48146821+Yoonsik-Kang@users.noreply.github.com>
2019-03-02 18:20:59 +1100

    Add files via upload

---
+++ react-static-browser-plugins.js
@@ -0,0 +1,17 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/kate/Developments/motor-junction/node_modules/react-static-plugin-styled-components",
+  plugins: [],
+  hooks: {}
+},
+{
+  location: "/Users/kate/Developments/motor-junction",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
